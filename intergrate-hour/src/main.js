0c02c1b7e9f96084b218b9df4d9accb6183272c1
xueeeeeeeeeeeeeeeeeeeee <122249998+xueeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2024-02-25 12:40:14 +0800

    create

--- intergrate-hour/src/main.js
+++ intergrate-hour/src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import router from './router';
+import 'ant-design-vue/dist/reset.css';
+import Antd from 'ant-design-vue';
+import zhCN from 'ant-design-vue/es/locale/zh_CN';
+const app = createApp(App)
+app.use(Antd);
 
-createApp(App).mount('#app')
+app.config.globalProperties.$antLocale = zhCN;
+app.use(router).mount('#app');
